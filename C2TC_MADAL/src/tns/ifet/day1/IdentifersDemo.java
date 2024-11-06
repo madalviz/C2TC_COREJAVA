bfd7f4fe1821e31bddc2b1918ff006aaf67970a0
BEST SOLUTION <BEST SOLUTION@DESKTOP-JLHKJMD>
2024-11-05 21:58:53 -0800

    update

---
+++ C2TC_MADAL/src/tns/ifet/day1/IdentifersDemo.java
@@ -0,0 +1,12 @@
+package tns.ifet.day1;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int $number=20;
+		System.out.println("value of the number variable is:"+$number);
+		String studentName="Aniket";
+		System.out.println("Value is:"+studentName);				
+	}
+
+}
